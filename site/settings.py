e882c1ba5b9aaf31e8bcbb125f2169251fb627d0
italamanova <iryna.talamanova@gmail.com>
2017-03-12 12:36:54 +0200

    refactor

--- site/settings.py
+++ site/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'kotha.urls'
+ROOT_URLCONF = 'site.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'kotha.wsgi.application'
+WSGI_APPLICATION = 'site.wsgi.application'
 
 
 # Database
